fda66f555856891eb30c27a382a92045f8707573
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2022-05-17 20:48:33 +0500

    add get list item func for getting item=of list by list id

--- src/services/SharePoint/SharePointService.ts
+++ src/services/SharePoint/SharePointService.ts
@@ -2,6 +2,7 @@ import { WebPartContext } from "@microsoft/sp-webpart-base";
 import { EnvironmentType } from "@microsoft/sp-core-library";
 import { SPHttpClient } from "@microsoft/sp-http";
 import { IListCollection } from "./IList";
+import { IListItemCollection } from "./IListItem";
 
 class SharePointServiceManager {
   public context: WebPartContext;
@@ -32,6 +33,17 @@ class SharePointServiceManager {
   public getLists(showHiddenList: boolean = false): Promise<IListCollection> {
     return this.get(
       `/_api/lists${!showHiddenList ? "?$filter=Hidden eq false" : ""}`
+    );
+  }
+
+  public getListItems(
+    listId: string,
+    selectedFields?: string[]
+  ): Promise<IListItemCollection> {
+    return this.get(
+      `/_api/lists/getbyid("${listId}")/items${
+        selectedFields ? `?$select=${selectedFields.join(",")}` : ""
+      }`
     );
   }
 }
